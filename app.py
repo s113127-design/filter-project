bcb54ddd7026532b453da2ade9f48f9771393aba
s113127-design <s113127@ms2.kghs.kh.edu.tw>
2026-06-13 19:08:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ class VideoProcessor:
                     #eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
                     tongue_y = int(
                         lower_lip.y * h
-                        - tongue_h * 0.5
+                        - tongue_h * 0.75
                     )
             
                     img = overlay_image(
