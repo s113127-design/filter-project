598b7c9e5874c505fc01d9fc74122330d8ec27f2
s113127-design <s113127@ms2.kghs.kh.edu.tw>
2026-06-13 19:34:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if "history" not in st.session_state:
 @st.cache_data
 def load_resources():
     e_hair = cv2.imread("assets/Einstein_hair.png", cv2.IMREAD_UNCHANGED)
-    e_tongue = cv2.imread("Einstein_tongue.png", cv2.IMREAD_UNCHANGED)
+    e_tongue = cv2.imread("assets/Einstein_tongue.png", cv2.IMREAD_UNCHANGED)
     return e_hair, e_tongue
 
 einstein_hair, einstein_tongue = load_resources()
