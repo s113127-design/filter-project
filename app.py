1a7c3aede506aebe43985ea96025c60c44abc8ba
s113127-design <s113127@ms2.kghs.kh.edu.tw>
2026-06-13 19:24:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ class VideoProcessor:
             
                     face_width = abs(right_face.x - left_face.x) * w
             
-                    hair_w = int(face_width * 2)
+                    hair_w = int(face_width * 1.8)
             
                     scale = (
                         einstein_hair.shape[0]
