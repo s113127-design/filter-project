2ea69f12093108c59c4d13fe7307982612d67c72
s113127-design <s113127@ms2.kghs.kh.edu.tw>
2026-06-13 19:05:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ class VideoProcessor:
                     
                     hair_y = int(
                         forehead.y * h
-                        - hair_h * 2
+                        - hair_h * 0.3
                     )
             
                     img = overlay_image(
@@ -147,7 +147,7 @@ class VideoProcessor:
                     #eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
                     tongue_y = int(
                         lower_lip.y * h
-                        + tongue_h * 3
+                        + tongue_h * 0.3
                     )
             
                     img = overlay_image(
