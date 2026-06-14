2bd3769057b36e702f2ec854a2aa2db75af9914d
113127 <s113127@ms2.kghs.kh.edu.tw>
2026-06-14 13:38:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ class VideoProcessor:
                         * w
                     )
                     
-                    tongue_w = int(mouth_width * 1.9)
+                    tongue_w = int(mouth_width * 1.75)
             
                     scale = (
                         einstein_tongue.shape[0]
@@ -147,7 +147,7 @@ class VideoProcessor:
                     #eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
                     tongue_y = int(
                         lower_lip.y * h
-                        - tongue_h * 0.5
+                        - tongue_h * 0.3
                     )
             
                     img = overlay_image(
